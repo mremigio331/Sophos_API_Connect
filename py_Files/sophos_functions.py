d91a94dca9fddd2ca348429da851756a2bbf5a2f
mremigio331 <76847029+mremigio331@users.noreply.github.com>
2022-03-21 10:22:07 -0700

    More info when logging

--- py_Files/sophos_functions.py
+++ py_Files/sophos_functions.py
@@ -43,9 +43,9 @@ def api_request(url, method='GET', params={}, headers={}, body=None, is_json=Tru
             note = ('Server failed to fulfill the request', e.code)
             sf.log_add(note, log_from, True)
         else:
-            note = e
+            note = str(e)
             sf.log_add(note, log_from, True)
-            
+
     return json.loads(response_body)
 
 def authenticate(client_id, client_secret):
